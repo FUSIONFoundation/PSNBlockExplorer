408c573250008e7f89907ae8066e9601774c0aca
bret-fusion <bret@fusion.org>
2018-12-22 12:06:34 -0500

    application menu implemented

--- src/Components/Header/AppSelect.js
+++ src/Components/Header/AppSelect.js
@@ -13,8 +13,46 @@ import NodeSelect from "../NodeSelect";
 import Header from "./Header";
 
 var fusionLogo = require("../../images/explorer-logo.svg");
-var boxes = require("./9Boxes.svg");
+var wallet = require("./wallet.svg");
+var asset = require("./asset.svg");
+var autobuy = require("./autobuy.svg");
+var block = require("./block.svg");
+var network = require("./network.svg");
+var swap = require("./swap.svg");
 
+class SelectItem extends Component {
+    state = {
+        hover : false
+    }
+    render( ) {
+        let bkc = this.state.hover ? colors.backgroundGrey : colors.white
+
+        return (
+            <TouchableOpacity onPress={()=>{
+
+                window.open( this.props.link )
+                Header.hideAppDisplay()
+            }}>
+            <View  onMouseEnter={()=>{
+                this.setState( { hover : true } )
+            }}
+            onMouseLeave={()=>{
+                this.setState( { hover : false } )
+            }}
+             style={{flex:1,flexDirection:'row',alignItems:'center',
+             marginBottom : 16,
+             justifyContent:'flex-start', backgroundColor : bkc}}>
+                 <Image
+                source={this.props.image}
+                resizeMode="contain"
+                style={{marginRight : 16, width: 14, height: 14 }}
+              />
+                <Text style={styles.selectItemText}>{this.props.text}</Text>
+            </View>
+            </TouchableOpacity>
+        )
+    }
+}
 export default class AppSelect extends Component {
  
   constructor( props ) {
@@ -36,7 +74,13 @@ export default class AppSelect extends Component {
       >
         <View style={styles.modalBackground}>
           <View style={styles.popupMenuBox}>
-            <Text>Apps</Text>
+            <Text style={styles.appsText}>Apps</Text>
+            <SelectItem image={wallet} text="My Wallet" link="https://www.myfusionwallet.com"/>
+            <SelectItem image={asset} text="Asset Gateway" link="https://assetgateway.fusionnetwork.io"/>
+            <SelectItem image={autobuy} text="Auto Buy Stake" link="https://tickets.fusionnetwork.io"/>
+            <SelectItem image={block} text="Block Explorer" link="https://blocks.fusionnetwork.io"/>
+            <SelectItem image={network} text="Network Monitor" link="https://node.fusionnetwork.io"/>
+            <SelectItem image={swap} text="Quantum Swap" link="https://swap.fusionnetwork.io"/>
           </View>
         </View>
       </TouchableWithoutFeedback>
@@ -65,6 +109,19 @@ var styles = StyleSheet.create({
     top: 64,
     left: 224,
     padding: 16
+  },
+  selectItemText : {
+    fontFamily: constants.fontFamily,
+    fontSize: 14,
+    fontWeight: constants.boldFont,
+    color: colors.textBlue,
+  },
+  appsText : {
+    fontFamily: constants.fontFamily,
+    fontSize: 14,
+    fontWeight: constants.regularFont,
+    color: colors.labelGrey,
+    marginBottom : 16
   },
   borderTitle: {
     width: 0.9,
