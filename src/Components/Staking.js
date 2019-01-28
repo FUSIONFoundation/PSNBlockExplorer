ea288cf01fdc38beb023d1fdd5c0e6c4a30a19c9
Bret <bret@fusion.org>
2019-01-27 20:25:54 -0500

    migrating staking screen to responsive display

--- src/Components/Staking.js
+++ src/Components/Staking.js
@@ -4,7 +4,8 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
 
 import "../App.css";
@@ -27,16 +28,22 @@ class BigButton extends Component {
           style={{
             width: 80,
             marginRight: 16,
+            marginTop : 8,
             height: 48,
             borderRadius: 3,
             borderWidth: 1,
             borderColor: colors.orderGrey,
-            backgroundColor: this.props.active
-              ? colors.disabledBlue
-              : colors.white
+            backgroundColor: this.props.active ? colors.textBlue : colors.white
           }}
         >
-          <Text style={styles.bigButtonText}>{this.props.text}</Text>
+          <Text
+            style={[
+              styles.bigButtonText,
+              { color: this.props.active ? colors.white : colors.textBlue }
+            ]}
+          >
+            {this.props.text}
+          </Text>
         </View>
       </TouchableOpacity>
     );
@@ -53,6 +60,7 @@ export default class Staking extends Component {
     FSN_Return: "-",
     Total_Amount: "-",
     Total_Return: "-",
+    width : parseInt( Dimensions.get("window").width * .45 ),
     ticketNumber: currentDataState.datablock.ticketNumber
   };
 
@@ -60,6 +68,9 @@ export default class Staking extends Component {
     super(props);
     this.calcDisplay = this.calcDisplay.bind(this);
     this.ticketNumberChanged = this.ticketNumberChanged.bind(this);
+
+
+    this.updateDimensions = this.updateDimensions.bind(this)
   }
 
   calcDisplay(valIn, cmdPast) {
@@ -70,8 +81,14 @@ export default class Staking extends Component {
     let ticketNumber = parseInt(this.state.ticketNumber);
 
     val = parseInt(val);
-    if (isNaN(val) || val <= 0 || isNaN(ticketNumber) || !valIn || valIn.length === 0) {
-      obj.stakeVal = valIn
+    if (
+      isNaN(val) ||
+      val <= 0 ||
+      isNaN(ticketNumber) ||
+      !valIn ||
+      valIn.length === 0
+    ) {
+      obj.stakeVal = valIn;
       obj.PFSN_Amount = "-";
       obj.FSN_Amount = "-";
       obj.PFSN_Return = "-";
@@ -104,27 +121,28 @@ export default class Staking extends Component {
     }
 
     //let averageBlockTime = 15;
-    let Total_Tickets =  ticketNumber
+    let Total_Tickets = ticketNumber;
     let User_FSN = val;
-    let User_Tickets = Math.floor(User_FSN/200)
+    let User_Tickets = Math.floor(User_FSN / 200);
 
     let Time_Invested = days;
     //let pFsnToFsnExchange = 1;
 
     let Blocks_Per_Day = 5760;
-    let New_Total_Tickets = Total_Tickets+User_Tickets
-    let Probability_Reward = User_Tickets/ (New_Total_Tickets)
-    let PFSN_Reward_Block = 2.5
-    let FSN_Reward_Block = .625
-    let Total_PFSN_Reward_Day_Possible = PFSN_Reward_Block*Blocks_Per_Day
-    let Total_FSN_Reward_Day_Possible = Blocks_Per_Day*FSN_Reward_Block
-    let PFSN_Reward_User = Time_Invested*Probability_Reward*Total_PFSN_Reward_Day_Possible
-    let ROR_PFSN = PFSN_Reward_User/User_FSN
-    let FSN_Reward_User =Time_Invested*Probability_Reward*Total_FSN_Reward_Day_Possible
-    let ROR_FSN =FSN_Reward_User/User_FSN
-    let FSN_PFSN_Reward_User = PFSN_Reward_User+FSN_Reward_User
-    let ROR_PFSN_PLUS_FSN = FSN_PFSN_Reward_User/User_FSN
-
+    let New_Total_Tickets = Total_Tickets + User_Tickets;
+    let Probability_Reward = User_Tickets / New_Total_Tickets;
+    let PFSN_Reward_Block = 2.5;
+    let FSN_Reward_Block = 0.625;
+    let Total_PFSN_Reward_Day_Possible = PFSN_Reward_Block * Blocks_Per_Day;
+    let Total_FSN_Reward_Day_Possible = Blocks_Per_Day * FSN_Reward_Block;
+    let PFSN_Reward_User =
+      Time_Invested * Probability_Reward * Total_PFSN_Reward_Day_Possible;
+    let ROR_PFSN = PFSN_Reward_User / User_FSN;
+    let FSN_Reward_User =
+      Time_Invested * Probability_Reward * Total_FSN_Reward_Day_Possible;
+    let ROR_FSN = FSN_Reward_User / User_FSN;
+    let FSN_PFSN_Reward_User = PFSN_Reward_User + FSN_Reward_User;
+    let ROR_PFSN_PLUS_FSN = FSN_PFSN_Reward_User / User_FSN;
 
     obj.PFSN_Amount = PFSN_Reward_User.toFixed(2);
     obj.FSN_Amount = FSN_Reward_User.toFixed(2);
@@ -140,10 +158,11 @@ export default class Staking extends Component {
 
   ticketNumberChanged(ticketNumber) {
     this.setState({ ticketNumber });
-    this.calcDisplay( this.state.stakeVal )
+    this.calcDisplay(this.state.stakeVal);
   }
 
   componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
     this.calcDisplay("1000");
     this.mounted = true;
     currentDataState.on("ticketNumber", this.ticketNumberChanged);
@@ -151,115 +170,166 @@ export default class Staking extends Component {
 
   componentWillUnmount() {
     this.mounted = false;
+    window.removeEventListener("resize", this.updateDimensions);
     currentDataState.removeEventListener(
       "ticketNumber",
       this.ticketNumberChanged
     );
   }
 
+  updateDimensions() {
+    this.setState({ width : parseInt( Dimensions.get("window").width * .45 ) } );
+  }
+
+
   render() {
     let cmd = this.state.cmd;
 
+    let w = this.state.width
+
+    if ( w < 320 ) {
+      w = 320
+    }
+
     return (
       <View style={styles.container}>
-        <View>
-          <View style={styles.youStakeRowText}>
-            <Text style={styles.youStakeText}>
-              {"Active Tickets: " + this.state.ticketNumber}{" "}
-            </Text>
-          </View>
-          <View style={styles.youStakeRowText}>
-            <Text style={styles.youStakeText}>You Stake</Text>
-            <Text style={styles.youStakeForText}>For</Text>
-          </View>
-          <View style={styles.youStakeRow}>
-            <View style={{ width: 220, marginRight : 16 }}>
-              <TextInput
-                style={[styles.stakeQuantityInput]}
-                placeholder="0"
-                autoCorrect={false}
-                placeholderTextColor={colors.orderGrey}
-                maxLength={10}
-                value={"" + (this.state.stakeVal || "")}
-                onChangeText={val => {
-                  this.calcDisplay(val);
-                }}
-              />
+        <View style={styles.innerContainer}>
+          <View
+            style={{
+              width: w,
+              height: 400,
+              backgroundColor: colors.tagGrey,
+              paddingTop: 16,
+              paddingBottom: 16,
+              paddingLeft: 24,
+              paddingRight: 24,
+              boxShadow: "0 6px 12px 0 rgba(0, 15, 33, 0.04)",
+              overflow: "visible"
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: colors.tagGrey
+              }}
+            >
+              <View style={styles.youStakeRowText}>
+                <Text style={styles.youStakeText}>
+                  {"Active Tickets: " + this.state.ticketNumber}{" "}
+                </Text>
+              </View>
+              <View style={styles.youStakeRowText}>
+                <Text style={styles.youStakeText}>Your Stake</Text>
+              </View>
+              <View style={styles.youStakeRow}>
+                <View style={{ width: 220, marginRight: 16 }}>
+                  <TextInput
+                    style={[styles.stakeQuantityInput]}
+                    placeholder="0"
+                    autoCorrect={false}
+                    placeholderTextColor={colors.orderGrey}
+                    maxLength={10}
+                    value={"" + (this.state.stakeVal || "")}
+                    onChangeText={val => {
+                      this.calcDisplay(val);
+                    }}
+                  />
+                </View>
+              </View>
+              <View style={{ marginTop: 32 }}>
+                <Text style={styles.youStakeForText}>Staking Time</Text>
+                <View style={{ height: 8 }} />
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "flex-start",
+                    flexWrap : 'wrap',
+                  }}
+                >
+                  <BigButton
+                    text="30d"
+                    active={cmd === "30d"}
+                    onPress={() => {
+                      this.calcDisplay(undefined, "30d");
+                    }}
+                  />
+                  <BigButton
+                    text="60d"
+                    active={cmd === "60d"}
+                    onPress={() => {
+                      this.calcDisplay(undefined, "60d");
+                    }}
+                  />
+                  <BigButton
+                    text="90d"
+                    active={cmd === "90d"}
+                    onPress={() => {
+                      this.calcDisplay(undefined, "90d");
+                    }}
+                  />
+                  <BigButton
+                    text="180d"
+                    active={cmd === "180d"}
+                    onPress={() => {
+                      this.calcDisplay(undefined, "180d");
+                    }}
+                  />
+                  <BigButton
+                    text="1yr"
+                    active={cmd === "1yr"}
+                    onPress={() => {
+                      this.calcDisplay(undefined, "1yr");
+                    }}
+                  />
+                </View>
+                <View style={{ height: 32 }} />
+              </View>
             </View>
-            <BigButton
-              text="30d"
-              active={cmd === "30d"}
-              onPress={() => {
-                this.calcDisplay(undefined, "30d");
-              }}
-            />
-            <BigButton
-              text="60d"
-              active={cmd === "60d"}
-              onPress={() => {
-                this.calcDisplay(undefined, "60d");
-              }}
-            />
-            <BigButton
-              text="90d"
-              active={cmd === "90d"}
-              onPress={() => {
-                this.calcDisplay(undefined, "90d");
-              }}
-            />
-            <BigButton
-              text="180d"
-              active={cmd === "180d"}
-              onPress={() => {
-                this.calcDisplay(undefined, "180d");
-              }}
-            />
-            <BigButton
-              text="1yr"
-              active={cmd === "1yr"}
-              onPress={() => {
-                this.calcDisplay(undefined, "1yr");
-              }}
-            />
-          </View>
-          <View style={{ height: 32 }} />
-          <Text style={styles.youReceiveText}>You Receive</Text>
-          <View style={styles.simpleRow}>
-            <Text style={[styles.smallLabelText, { width: 200 }]}>Token</Text>
-            <Text style={[styles.smallLabelText, { width: 200 }]}>Amount</Text>
-            <Text style={[styles.smallLabelText, { width: 200 }]}>
-              Annualized Return
-            </Text>
-          </View>
-          <View style={styles.simpleRow}>
-            <Text style={styles.calcLabelText}>P-FSN</Text>
-            <Text style={styles.calcAmountText}>{this.state.PFSN_Amount}</Text>
-            <Text style={styles.calcReturnText}>
-              {utils.formatPercent(this.state.PFSN_Return)}
-            </Text>
-          </View>
-          <Text style={styles.plusText}>+</Text>
-          <View style={styles.simpleRow}>
-            <Text style={styles.calcLabelText}>FSN</Text>
-            <Text style={styles.calcAmountText}>{this.state.FSN_Amount}</Text>
-            <Text style={styles.calcReturnText}>
-              {utils.formatPercent(this.state.FSN_Return)}
-            </Text>
           </View>
           <View
             style={{
-              width: 736,
-              height: 1,
+              width: w,
+              height: 400,
               backgroundColor: colors.white,
-              marginTop: 16
+              paddingTop: 16,
+              paddingBottom: 16,
+              paddingLeft: 24,
+              paddingRight: 24,
+              boxShadow: "0 6px 12px 0 rgba(0, 15, 33, 0.04)",
+              overflow: "visible"
             }}
-          />
-          <View style={styles.simpleRow}>
-            <Text style={styles.calcLabelText}>~FSN</Text>
-            <Text style={styles.calcAmountText}>{this.state.Total_Amount}</Text>
-            <Text style={styles.calcReturnText}>
-              {utils.formatPercent(this.state.Total_Return)}
-            </Text>
+          >
+            <View
+              style={{
+                backgroundColor: "white"
+              }}
+            >
+              <Text style={styles.youReceiveText}>Your Estimated Return</Text>
+              <View style={{ height: 16 }} />
+              <View style={styles.simpleRow}>
+                <Text style={styles.calcAmountText}>
+                  {this.state.PFSN_Amount}
+                </Text>
+                <Text style={styles.smallLabelText}>P-FSN</Text>
+                <View style={{ width: 32 }} />
+                <Text style={styles.calcReturnText}>
+                  {utils.formatPercent(this.state.PFSN_Return)}
+                </Text>
+                <Text style={styles.smallLabelText}>ROR</Text>
+              </View>
+              <View style={styles.simpleRow}>
+                <Text style={styles.calcAmountText}>
+                  {this.state.FSN_Amount}
+                </Text>
+                <Text style={styles.smallLabelText}>FSN</Text>
+                <View style={{ width: 32 }} />
+                <Text style={styles.calcReturnText}>
+                  {utils.formatPercent(this.state.FSN_Return)}
+                </Text>
+                <Text style={styles.smallLabelText}>ROR</Text>
+              </View>
+              <View style={{ height: 48 }} />
+            </View>
           </View>
         </View>
       </View>
@@ -273,12 +343,19 @@ styles = StyleSheet.create({
     flexGrow: 0,
     flexShrink: 0,
     flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    backgroundColor: colors.primaryBlue,
-    padding: 64,
-    height: 1200,
-    width: 1200
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.white
+  },
+  innerContainer: {
+    flex: 1,
+    flexGrow: 0,
+    flexShrink: 0,
+    flexDirection: "row",
+    justifyContent : 'space-evenly',
+    alignItems : 'center',
+    backgroundColor: colors.white,
+    flexWrap: "wrap"
   },
   youStakeRowText: {
     flex: 1,
@@ -288,10 +365,10 @@ styles = StyleSheet.create({
   },
   youStakeText: {
     fontFamily: constants.fontFamily,
-    fontSize: 20,
-    color: colors.white,
+    fontSize: 18,
+    color: colors.textBlue,
     width: 240,
-    fontWeight: constants.mediumFont
+    fontWeight: constants.regularFont
   },
   titleText: {
     fontFamily: constants.fontFamily,
@@ -302,21 +379,22 @@ styles = StyleSheet.create({
   },
   youStakeForText: {
     fontFamily: constants.fontFamily,
-    fontSize: 20,
-    color: colors.white,
-    fontWeight: constants.mediumFont
+    fontSize: 13,
+    color: colors.texBlue,
+    fontWeight: constants.regularFont
   },
   youReceiveText: {
     fontFamily: constants.fontFamily,
     fontSize: 18,
-    color: colors.white,
-    fontWeight: constants.mediumFont
+    color: colors.textBlue,
+    fontWeight: constants.regularFont
   },
   smallLabelText: {
     fontFamily: constants.fontFamily,
     fontSize: 12,
-    color: colors.white,
-    fontWeight: constants.mediumFont
+    color: colors.textBlue,
+    fontWeight: constants.mediumFont,
+    paddingTop: 9
   },
   simpleRow: {
     marginTop: 16,
@@ -327,32 +405,32 @@ styles = StyleSheet.create({
   plusText: {
     fontFamily: constants.fontFamily,
     fontSize: 24,
-    color: colors.white,
+    color: colors.textBlue,
     fontWeight: constants.boldFont
   },
   calcLabelText: {
     fontFamily: constants.fontFamily,
     fontSize: 24,
-    color: colors.white,
+    color: colors.textBlue,
     fontWeight: constants.mediumFont,
     width: 100
   },
   calcAmountText: {
     fontFamily: constants.fontFamily,
-    fontSize: 24,
-    color: colors.white,
+    fontSize: 22,
+    color: colors.textBlue,
     fontWeight: constants.mediumFont,
-    width: 150,
-    marginRight: 150,
-    textAlign: "right"
+    marginRight: 4,
+    textAlign: "left"
   },
   calcReturnText: {
     fontFamily: constants.fontFamily,
-    fontSize: 24,
-    color: colors.white,
+    fontSize: 14,
+    color: colors.textBlue,
     fontWeight: constants.mediumFont,
-    width: 90,
-    textAlign: "right"
+    marginRight: 4,
+    textAlign: "left",
+    paddingTop: 7
   },
   youStakeRow: {
     flex: 1,
@@ -392,7 +470,7 @@ styles = StyleSheet.create({
     color: colors.textBlue,
     textAlign: "center",
     padding: 16,
-    marginBottom: 8
+    marginBottom: 14
   },
   info: {
     fontSize: 16,
